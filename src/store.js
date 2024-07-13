ab68a36c37f15979507aa132cb3283b58227ed51
krAlena <krasnoshchoka@bk.ru>
2024-07-13 14:01:48 +0300

    build

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CreateSlice';
-
- const store = configureStore({
-    reducer: {
-        cart: cartReducer
-    },
-});
-export default store
